7027357df79ed91cbb182948829cb0d8d2f2eaa4
egee-irl <brian@egee.io>
2021-07-31 22:50:51 -0700

    basically rewrite the whole thing

--- gsdcli/gsdcli.py
+++ gsdcli/gsdcli.py
@@ -1,106 +1,88 @@
+#!/usr/bin/env python3
 import os
-import yaml 
+import yaml
 import click
 import getpass
+import subprocess
 import urllib.request
 
+from time import time
+
+
+class GameManager:
+    def __init__(self, name) -> None:
+        with open(f"{os.path.dirname(__file__)}/servers.yaml") as f:
+            serverObj = yaml.load(f, Loader=yaml.FullLoader)[name]
+        f.close()
+
+        self.name = serverObj["name"]
+        self.installer = serverObj["installer"]
+        self.backend = serverObj["backend"]
+        self.appId = serverObj["appId"]
+        self.params = serverObj["launchParams"]
+        self.user = getpass.getuser()
+        self.install_path = f'{os.path.expanduser("~")}/{self.name}-server'
+        self.daemon_path = f'{os.path.expanduser("~")}/.config/systemd/user'
+        self.launch_file = f"{self.install_path}/launch.sh"
+        self.steamcmd_path = "/opt/steam"
+        os.makedirs(self.daemon_path, exist_ok=True)
+
+    def install(self) -> None:
+        os.makedirs(self.install_path, exist_ok=True)
+        unit_file = f"{self.daemon_path}/{self.name}.service"
+
+        if os.path.exists(self.launch_file):
+            os.rename(self.launch_file, f"{self.launch_file}_{time()}.backup")
+
+        with open(self.launch_file, "w+") as file:
+            file.write(self.params)
+        file.close()
+
+        if os.path.exists(unit_file):
+            os.rename(unit_file, f"{unit_file}_{time()}.backup")
+        with open(unit_file, "w+") as file:
+            file.write(
+                f"[Unit]\nAfter=network.target\nDescription=Daemon for {self.name} dedicated server\n[Install]\nWantedBy=default.target\n[Service]\nType=simple\nWorkingDirectory={self.install_path}\nExecStart=/bin/bash {self.install_path}/launch.sh"
+            )
+        file.close()
+
+        if self.installer == "steamcmd":
+            subprocess.run(
+                [
+                    f"{self.steamcmd_path}/steamcmd.sh",
+                    "+login",
+                    "anonymous",
+                    "+force_install_dir",
+                    self.install_path,
+                    "+app_update",
+                    self.appId,
+                    "validate",
+                    "+quit",
+                ],
+                check=True,
+                cwd=self.steamcmd_path,
+            )
+
+        if self.backend == "srcds":
+            subprocess.run(["touch", f"{self.install_path}/{self.name}/console.log"])
+
+            with open(self.launch_file, "a") as file:
+                file.write(f"\ntail -f {self.launch_file}")
+            file.close()
+
+
 @click.group()
-def gsdcli():
-    pass
+@click.argument("name")
+@click.pass_context
+def gsdcli(ctx, name):
+    ctx.obj = GameManager(name)
+
 
 @gsdcli.command()
-@click.argument('name')
-def install(name):
-    serverObj = parseSupported(name)
+@click.pass_context
+def install(ctx):
+    ctx.obj.install()
 
-    if not os.path.exists(serverObj.get('install-path')):
-        createDirectories(serverObj)
-    createLaunchScript(serverObj)
-    createUnitFile(serverObj)
-
-    if serverObj.get('backend') == 'srdcs':
-        srdcsInstall(serverObj)
-    elif serverObj.get('backend') == 'scp':
-        steamCmdInstall(serverObj)
-        urllib.request.urlretrieve('https://github.com/Grover-c13/MultiAdmin/releases/download/3.2.5/MultiAdmin.exe', f"{serverObj.get('install-path')}/MultiAdmin.exe")
-    elif serverObj.get('backend') == 'unity':
-        steamCmdInstall(serverObj)
-        urllib.request.urlretrieve('https://github.com/Grover-c13/MultiAdmin/releases/download/3.2.5/MultiAdmin.exe', f"{serverObj.get('install-path')}/MultiAdmin.exe")
-    else:
-        exit(f"{serverObj.get('backend')} not supported yet.")
-
-##### Private Functions #####
-
-def parseSupported(name):
-    serverYaml = open(f'{os.path.dirname(__file__)}/_servers.yaml')
-    serverList = yaml.load(serverYaml, Loader=yaml.FullLoader)
-    serverObj = serverList.get(name)
-    if serverObj :
-        configureMetadata(name, serverObj)
-        return serverObj
-    else:
-        exit(f'{name} Not supported')
-
-def configureMetadata(name, serverObj):
-    serverObj.update({'friendly-name' : name})
-    serverObj.update({'user' : getpass.getuser()})
-    serverObj.update({'install-path' : f'{os.path.expanduser("~")}/{name}-server'})
-    serverObj.update({'systemd-path' : f'{os.path.expanduser("~")}/.config/systemd/user'})
-    serverObj.update({'dryrun' : 'ye'})
-
-def createDirectories(serverObj):
-    print('Creating prerequisite directories...')
-    os.mkdir(serverObj.get('install-path'))
-    if not os.path.exists(serverObj.get('systemd-path')):
-        os.mkdir(serverObj.get('systemd-path'))
-
-def createLaunchScript(serverObj):
-    print('Creating launch script...')
-    launchFilePath = f"{serverObj.get('install-path')}/launch.sh"
-    if os.path.exists(launchFilePath):
-        os.rename(launchFilePath, f"{launchFilePath}.backup")
-    launchFile = open(launchFilePath, "w+")
-    launchFile.write(serverObj.get('launchParams'))
-    launchFile.close()
-
-def createUnitFile(serverObj):
-    print('Creating unit file...')
-    unitFilePath = f"{serverObj.get('systemd-path')}/{serverObj.get('friendly-name')}.service"
-    if os.path.exists(unitFilePath):
-        os.rename(unitFilePath, f"{unitFilePath}.backup")
-    launchFile = open(unitFilePath, "w+")
-    launchFile.write(f"[Unit]\nAfter=network.target\nDescription=Daemon for {serverObj.get('friendly-name')} dedicated server\n[Install]\nWantedBy=default.target\n[Service]\nType=simple\nWorkingDirectory={serverObj.get('install-path')}\nExecStart=/bin/bash {serverObj.get('install-path')}/launch.sh")
-    launchFile.close()
-
-def steamLoginParser(serverObj):
-    if not serverObj.get('steamUserName'):
-        serverObj.update({'steamUserName' : 'anonymous'})
-        serverObj.update({'steamPassword' : ''})
-    return f"+login {serverObj.get('steamUserName')} {serverObj.get('steamPassword')}"
-
-def steamCmdInstall(serverObj):
-    steamLogin = steamLoginParser(serverObj)
-    installCmd = f"{steamLogin} +force_install_dir {serverObj.get('install-path')} +app_update {serverObj.get('appId')} validate +quit"
-    os.system(f'steamcmd {installCmd}')
-
-def srdcsInstall(serverObj):
-    steamCmdInstall(serverObj)
-    logFilePath = f"{serverObj.get('install-path')}/{serverObj.get('name')}/console.log"
-    launchFilePath = f"{serverObj.get('install-path')}/launch.sh"
-
-    logFile = open(logFilePath, 'a')
-    logFile.close()
-    
-    launchFile = open(launchFilePath, 'a')
-    launchFile.write(f"\ntail -f {logFilePath}")
-    launchFile.close()
-
-def unityHandler(serverObj):
-    if serverObj.get('name') == 'rust':
-        pass
-    elif serverObj.get('name') == 'sdtd':
-        pass
 
 if __name__ == "__main__":
-    # For Snapcraft
     gsdcli()
